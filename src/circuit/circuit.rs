418fbaa2be822553cc9125e0ae0ba22cae1bb635
Cedric Schwyter <cedricschwyter@bluewin.ch>
2022-08-28 14:30:27 +0200

    feature: add compute pool

--- src/circuit/circuit.rs
+++ src/circuit/circuit.rs
@@ -1,16 +1,16 @@
-use crossbeam::channel::{Receiver, Sender};
-use rayon::Scope;
+use crossbeam::channel::{bounded, Receiver, Sender};
+use rayon::{self, Scope};
 use std::error::Error;
 
 #[derive(Clone, Copy)]
-enum LogicLevel {
+pub enum LogicLevel {
     ZERO = 0,
     ONE = 1,
 }
 
-type InWire = Receiver<LogicLevel>;
-type OutWire = Sender<LogicLevel>;
-type Wire = (OutWire, InWire);
+pub type InWire = Receiver<LogicLevel>;
+pub type OutWire = Sender<LogicLevel>;
+pub type Wire = (OutWire, InWire);
 
 /// A circuit component intended to represent only a simple logic gate function with one output,
 /// however this implementation would also allow for more complex logic functions with multiple outputs.
@@ -20,20 +20,40 @@ struct LogicGate {
     logic: Box<dyn Fn(Vec<LogicLevel>) -> Vec<LogicLevel>>,
 }
 
-/// A recursive type to represent a combinational and/or a sequential circuit
+/// A recursive type to represent a combinational and/or a sequential circuit element.
 /// Inputs and outputs are dangling in their respective directions until they are reconciled. A
 /// protocol will be defined for that. In said protocol for each input/output, since every output
 /// is linked to another Circuit's input, one whole channel will be dropped per connection.
-struct Circuit<T: Propagatable + Send + Sync> {
+struct CircuitElement<T: Propagatable + Send + Sync> {
     inputs: Vec<Wire>,
     outputs: Vec<Wire>,
     circuits: Vec<Box<T>>,
     cutoff: bool,
 }
 
+/// Top level circuit object
+pub struct Circuit<T: Propagatable + Send + Sync> {
+    circuits: Vec<Box<T>>,
+}
+
+impl<T: Propagatable + Send + Sync> Circuit<T> {
+    /// Computes the next state from the current state. May utilize multiple CPUs to do so using
+    /// task parallelism.
+    pub fn compute(&mut self) -> Result<(), Box<dyn Error>> {
+        rayon::scope(|s| {
+            for circuit in self.circuits.iter_mut() {
+                // TODO: figure out how to gracefully handle Result types
+                circuit.propagate(s).unwrap();
+            }
+        });
+
+        Ok(())
+    }
+}
+
 /// A trait to propagate state computation through circuits. This computation is broken up into
 /// multiple tasks and implemented using task parallelism.
-trait Propagatable {
+pub trait Propagatable {
     fn propagate<'a>(&'a mut self, s: &Scope<'a>) -> Result<(), Box<dyn Error>>;
 }
 
@@ -54,7 +74,7 @@ impl Propagatable for LogicGate {
     }
 }
 
-impl<T: Propagatable + Send + Sync> Propagatable for Circuit<T> {
+impl<T: Propagatable + Send + Sync> Propagatable for CircuitElement<T> {
     fn propagate<'a>(&'a mut self, s: &Scope<'a>) -> Result<(), Box<dyn Error>> {
         if self.cutoff {
             for circuit in self.circuits.iter_mut() {
